7898a4df7ccc174499844529774baad9309c4748
PenagozDev <cristianpenagoso01@gmail.com>
2025-05-25 19:17:00 -0500

    add facebook button

---
+++ penagoszdev-resume/src/components/atom/social/instagram/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Icon } from '@iconify/react/dist/iconify.js'
+
+const index = () => {
+  return (
+    <div>
+        <a href="https://www.instagram.com/penagozdev/">
+        <Icon icon="mdi:instagram" width="24" height="24" />
+        </a>
+    </div>
+  )
+}
+
+export default index
--- penagoszdev-resume/src/pages/index.tsx
+++ penagoszdev-resume/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Face from '@/components/atom/social/face'
+import Instagram from '@/components/atom/social/instagram'
 
 const index = () => {
   return (
     <div>
+      <Face />  
+      <Instagram />
 
-      
       index
     </div>
   )
